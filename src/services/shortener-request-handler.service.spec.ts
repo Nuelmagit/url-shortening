6f5fd945f632408523b16f6f8e95db6690cfa3e2
Manuel Panizzo <manuel.panizzo@saritasa.com>
2021-09-26 19:22:53 -0300

    add test for validating service calls in request handler

--- src/services/shortener-request-handler.service.spec.ts
+++ src/services/shortener-request-handler.service.spec.ts
@@ -2,10 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EncoderModule } from './encoders/url-encoder.module';
 import { ShortenerRequestHandler } from './shortener-request-handler.service';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { DECODE_EVENT, ENCODE_EVENT } from 'src/events/events';
+import { UrlEncoderService } from './encoders/url-encoder.service';
+
 const FIRST_ENCODED_URL = '84210vf731';
 
 describe('ShortenerRequestHandler', () => {
   let shortenerRequestHandler: ShortenerRequestHandler;
+  let eventEmitter: EventEmitter2;
+  let urlEncoderService: UrlEncoderService;
+
   const EXAMPLE_URL = 'www.google.com';
 
   beforeEach(async () => {
@@ -17,6 +24,10 @@ describe('ShortenerRequestHandler', () => {
     shortenerRequestHandler = module.get<ShortenerRequestHandler>(
       ShortenerRequestHandler,
     );
+
+    eventEmitter = module.get<EventEmitter2>(EventEmitter2);
+
+    urlEncoderService = module.get<UrlEncoderService>(UrlEncoderService);
   });
 
   it('ShortenerRequestHandler - should be defined', () => {
@@ -40,5 +51,58 @@ describe('ShortenerRequestHandler', () => {
     });
 
     expect(decodedResponse.longUrl).toBe(EXAMPLE_URL);
+  });
+
+  it('Should emit encode event', () => {
+    const spy = jest.spyOn(eventEmitter, 'emit');
+
+    shortenerRequestHandler.handleEncode({
+      longUrl: EXAMPLE_URL,
+    });
+
+    expect(spy).toBeCalledWith(ENCODE_EVENT, {
+      encodedUrl: EXAMPLE_URL,
+      recentlyEncoded: true,
+    });
+  });
+
+  it('Should emit decode event', () => {
+    const encodeResponse = shortenerRequestHandler.handleEncode({
+      longUrl: EXAMPLE_URL,
+    });
+
+    const spy = jest.spyOn(eventEmitter, 'emit');
+
+    shortenerRequestHandler.handleDecode({
+      shortUrl: encodeResponse.shortUrl,
+    });
+
+    expect(spy).toBeCalledWith(DECODE_EVENT, {
+      decodedUrl: EXAMPLE_URL,
+    });
+  });
+
+  it('Should call encodeLongUrl of urlEncoderService with the longUrl', () => {
+    const spy = jest.spyOn(urlEncoderService, 'encodeLongUrl');
+
+    shortenerRequestHandler.handleEncode({
+      longUrl: EXAMPLE_URL,
+    });
+
+    expect(spy).toBeCalledWith(EXAMPLE_URL);
+  });
+
+  it('Should call decodeShortUrl of urlEncoderService with the shortUrl', () => {
+    const encodeResponse = shortenerRequestHandler.handleEncode({
+      longUrl: EXAMPLE_URL,
+    });
+
+    const spy = jest.spyOn(urlEncoderService, 'decodeShortUrl');
+
+    shortenerRequestHandler.handleDecode({
+      shortUrl: encodeResponse.shortUrl,
+    });
+
+    expect(spy).toBeCalledWith(encodeResponse.shortUrl);
   });
 });
