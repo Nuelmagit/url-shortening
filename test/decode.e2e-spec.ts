ac3992fe8b74ab30e87cd5856e8b4c5405fbc878
Manuel Panizzo <manuel.panizzo@saritasa.com>
2021-09-28 20:10:40 -0300

    add decode e2e test

---
+++ test/decode.e2e-spec.ts
@@ -0,0 +1,75 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+const BAD_REQUEST_BODY = {
+  statusCode: 400,
+  message: ['shortUrl must contain only letters and numbers'],
+  error: 'Bad Request',
+};
+
+const notFoundBody = (url: string) => {
+  return {
+    statusCode: 404,
+    message: `${url} does not exist`,
+    error: 'Not Found',
+  };
+};
+
+/**
+ * TODO. exhaustive test
+ */
+describe('ShortUrlController - decode (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+  });
+
+  it('Should return Bad request when empty body', () => {
+    return request(app.getHttpServer())
+      .post('/decode')
+      .expect(400)
+      .expect(BAD_REQUEST_BODY);
+  });
+
+  it('Should return Not found when short url is not valid / does not exist', () => {
+    return request(app.getHttpServer())
+      .post('/decode')
+      .send({ shortUrl: 'abc' })
+      .expect(404)
+      .expect(notFoundBody('abc'));
+  });
+
+  it('Should return Bad request when url is empty', () => {
+    return request(app.getHttpServer())
+      .post('/decode')
+      .send({ shortUrl: null })
+      .expect(400)
+      .expect(BAD_REQUEST_BODY);
+  });
+
+  it('Should return shortUrl in the response body', () => {
+    const longUrl = 'https://www.google.com';
+
+    const decodeTest = (shortUrl: string) =>
+      request(app.getHttpServer())
+        .post('/decode')
+        .send({ shortUrl })
+        .expect(200)
+        .expect({ longUrl });
+
+    return request(app.getHttpServer())
+      .post('/encode')
+      .send({ longUrl })
+      .then((okResponse) => okResponse.body.shortUrl)
+      .then(decodeTest);
+  });
+});
