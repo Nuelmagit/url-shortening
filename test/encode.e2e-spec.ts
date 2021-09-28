b52ef761a603196a5c366f19ab4917049a0440f0
Manuel Panizzo <manuel.panizzo@saritasa.com>
2021-09-28 20:11:01 -0300

    add encode e2e test

---
+++ test/encode.e2e-spec.ts
@@ -0,0 +1,68 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+const BAD_REQUEST_BODY = {
+  statusCode: 400,
+  message: ['longUrl must be an URL address'],
+  error: 'Bad Request',
+};
+
+const FIRST_URL_RESPONSE_BODY = { shortUrl: '84210vf731' };
+
+/**
+ * TODO. exhaustive test
+ */
+describe('ShortUrlController - encode (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+  });
+
+  it('Should return Bad request when empty body', () => {
+    return request(app.getHttpServer())
+      .post('/encode')
+      .expect(400)
+      .expect(BAD_REQUEST_BODY);
+  });
+
+  it('Should return Bad request when url is not valid', () => {
+    return request(app.getHttpServer())
+      .post('/encode')
+      .send({ longUrl: 'abc' })
+      .expect(400)
+      .expect(BAD_REQUEST_BODY);
+  });
+
+  it('Should return Bad request when url is empty', () => {
+    return request(app.getHttpServer())
+      .post('/encode')
+      .send({ longUrl: null })
+      .expect(400)
+      .expect(BAD_REQUEST_BODY);
+  });
+
+  it('Should return Bad request when the url has no protocol', () => {
+    return request(app.getHttpServer())
+      .post('/encode')
+      .send({ longUrl: 'www.google.com' })
+      .expect(400)
+      .expect(BAD_REQUEST_BODY);
+  });
+
+  it('Should return shortUrl in the response body', () => {
+    return request(app.getHttpServer())
+      .post('/encode')
+      .send({ longUrl: 'https://www.google.com' })
+      .expect(200)
+      .expect(FIRST_URL_RESPONSE_BODY);
+  });
+});
