caeaebaefa42f3fabb0898338e01ffac54563564
Manuel Panizzo <manuel.panizzo@saritasa.com>
2021-09-28 00:30:16 -0300

    add missing listener to const

--- src/services/event-listeners/listeners.ts
+++ src/services/event-listeners/listeners.ts
@@ -1,4 +1,9 @@
 import { GeneralDecodeListener } from './general-decode.listener';
 import { GeneralEncodeListener } from './general-encode.listener';
+import { UrlStatisticListener } from './url-statistic.listener';
 
-export const listeners = [GeneralEncodeListener, GeneralDecodeListener];
+export const listeners = [
+  GeneralEncodeListener,
+  GeneralDecodeListener,
+  UrlStatisticListener,
+];
