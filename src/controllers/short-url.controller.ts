b003df98c9d965a47c79585d3a6a8a5cc22580c2
Manuel Panizzo <manuel.panizzo@saritasa.com>
2021-09-28 21:16:53 -0300

    concat request path to short url

--- src/controllers/short-url.controller.ts
+++ src/controllers/short-url.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Req } from '@nestjs/common';
 import { DecodeDto } from 'src/dtos/decode.dto';
 import { EncodeDto } from 'src/dtos/encode.dto';
 import { DecodeResponse } from 'src/response-types/decode-response.dto';
 import { EncodeResponse } from 'src/response-types/encode-response.dto';
 import { ShortenerRequestHandler } from 'src/services/shortener-request-handler.service';
-
+import { Request } from 'express';
 @Controller()
 export class ShortUrlController {
   constructor(
@@ -13,13 +13,24 @@ export class ShortUrlController {
 
   @Post('encode')
   @HttpCode(200)
-  encode(@Body() encodeDto: EncodeDto): EncodeResponse {
-    return this.shortenerRequestHandler.handleEncode(encodeDto);
+  encode(
+    @Body() encodeDto: EncodeDto,
+    @Req() request: Request,
+  ): EncodeResponse {
+    const encodeResponse = this.shortenerRequestHandler.handleEncode(encodeDto);
+    this.concatPath(encodeResponse, request);
+    return encodeResponse;
   }
 
   @Post('decode')
   @HttpCode(200)
   decode(@Body() decodeDto: DecodeDto): DecodeResponse {
     return this.shortenerRequestHandler.handleDecode(decodeDto);
+  }
+
+  private concatPath(encodeResponse: EncodeResponse, request: Request) {
+    encodeResponse.shortUrl = `${request.protocol}://${request.get('host')}/${
+      encodeResponse.shortUrl
+    }`;
   }
 }
